d5ab1ffbaf5abb2c138393abac04fa05eb9c4fb6
Geovanna <138888795+rodriguesgeovanna@users.noreply.github.com>
2023-09-01 20:46:19 -0300

    Add files via upload

---
+++ slideShow.js
@@ -0,0 +1,40 @@
+const images = [
+    { 'id': '1', 'url': './img/chrono.jpg' },
+    { 'id': '2', 'url': './img/inuyasha.jpg' },
+    { 'id': '3', 'url': './img/ippo.png' },
+    { 'id': '4', 'url': './img/tenchi.jpg' },
+    { 'id': '5', 'url': './img/tenjhotenge.jpg' },
+    { 'id': '6', 'url': './img/yuyuhakusho.jpg' }
+];
+
+const containerItens = document.querySelector('#container-slide');
+
+const loadImages = (images, container) => {
+
+    images.forEach(images => {
+        container.innerHTML +=
+            `<div class='item'>
+             <img src='${image.url}'
+             </div>`
+
+    });
+};
+
+loadImages(images, containerItens);
+
+let items = document.querySelectorAll('.item');
+
+const anterior = () => {
+    containerItens.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const proximo = () => {
+    const lastItem = items[items.length - 1];
+    containerItens.insertBefore(lastItem, items[0]);
+    items = document.querySelectorAll('.item');
+
+};
+
+document.querySelector('#previous').addEventListener('click',anterior);
+document.querySelector('#next').addEventListener('click',proximo);
